cbc629e40a57583c72f35dbf4bfa42827f87851d
BuildTools <unconfigured@null.spigotmc.org>
2019-10-29 13:54:21 -0500

    Created Mod. Doesn't do anything for now.

---
+++ src/main/java/com/haxalicious/randomitegems/proxy/CommonProxy.java
@@ -0,0 +1,5 @@
+package com.haxalicious.randomitegems.proxy;
+
+public class CommonProxy {
+
+}
---
+++ src/main/java/com/haxalicious/randomitegems/util/Reference.java
@@ -0,0 +1,9 @@
+package com.haxalicious.randomitegems.util;
+
+public class Reference {
+	public static final String MODID = "randomitegemsreborn";
+	public static final String NAME = "Randomite Gems Reborn";
+	public static final String VERSION = "0.1";
+	public static final String CLIENT = "com.haxalicious.randomitegems.proxy.ClientProxy";
+	public static final String COMMON = "com.haxalicious.randomitegems.proxy.CommonProxy";
+}
