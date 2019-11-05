d3ff356d84bb62ed1e953259a59e6b5b2314a541
BuildTools <unconfigured@null.spigotmc.org>
2019-11-05 13:46:36 -0600

    Added texture to Randomite Gem.

--- src/main/java/com/haxalicious/randomitegems/util/Reference.java
+++ src/main/java/com/haxalicious/randomitegems/util/Reference.java
@@ -2,7 +2,7 @@ package com.haxalicious.randomitegems.util;
 
 // Reference Class
 public class Reference {
-	public static final String MODID = "randomitegemsreborn";
+	public static final String MODID = "randomitegems";
 	public static final String NAME = "Randomite Gems Reborn";
 	public static final String VERSION = "0.1";
 	public static final String CLIENT = "com.haxalicious.randomitegems.proxy.ClientProxy";
